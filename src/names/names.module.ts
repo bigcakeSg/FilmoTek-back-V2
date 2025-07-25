eaa942d76e6ac5a99e15563861ab372883fed360
Sébastien Girard <95628952+bigcakeSg@users.noreply.github.com>
2025-07-25 10:13:27 +0200

    get names/api-data (#5)

--- src/names/names.module.ts
+++ src/names/names.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { NamesController } from './names.controller';
 import { NamesService } from './names.service';
 import { NamesProviders } from './names.providers';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HttpModule],
   controllers: [NamesController],
   providers: [NamesService, ...NamesProviders],
 })
