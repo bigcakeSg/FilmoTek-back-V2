ca97c6e575b501c3df92f7a4ad2a0986d9557d8a
bigcakeSg <sebastiengirard@yahoo.fr>
2025-08-01 11:48:07 +0200

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  app.enableCors({
+    origin: true, // or a table with authorized origins ['http://localhost:5173']
+    // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
